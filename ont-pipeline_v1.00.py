6c0d2ed11f6e61651fda63ab5817b48e97910242
andrewdmarques <andrewdmarques@gmail.com>
2024-11-15 11:59:16 -0500

    2024-11-15 11:59am

---
+++ ont-pipeline_v1.00.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#############################################
+# Import packages
+#############################################
+import os  # For file management
+
+
+#############################################
+# User defined variables
+#############################################
+
+dir_data = "/var/lib/minknow/data/ONT-02/no_sample_id/20241111_1419_MN23638_FAZ97636_6cd4fac2/fastq_pass"
+prefix = "2024-11-14"
+dir_working = "/media/andrewdmarques/Data011/Bioinformatics/49_ONT-Processing/" + prefix
+
+
+# Run
+
+# Determine which files are present in the directory.
+
